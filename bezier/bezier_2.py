b643a86feadab085dfa0845dfa0d13c2d025e052
AntoineRichard <antoine.richard@uni.lu>
2024-11-06 23:07:40 +0100

    added mode to deal with non fixed number of keypoints per track.

--- bezier/bezier_2.py
+++ bezier/bezier_2.py
@@ -142,6 +142,100 @@ class TrackGenerator:
         ang = self._p * ang1 + (1 - self._p) * ang2 + (torch.abs(ang2 - ang1) > np.pi) * np.pi
         return points[:, :-1], ang
 
+    def get_curve_tangents_non_fixed_points(self, points: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+        """Given a set of points, compute the tangents of the curve that passes through them.
+        Unlike get_curve_tangents, this function will prune a random number of points from the input points tensor.
+        The method to achieve this is convoluted, if we were looping around all the tensor elements it would be straight
+        forward, but here we want to perform tensorized operations only. Here is a step-by-step explanation:
+
+        To make the code more efficient, the first point cannot be pruned. This is so that it's easy to set the removed
+        points to the first point in the sequence.
+
+        1. We first generate a tensor which stores the ids of all the points. We will use this to index the points tensor.
+        2. We then need to create a mask with the same shape as the ids tensor, that will tell us which points to prune.
+          2.a. We generate a binary tensor of length [num_points, max_points_to_prune] with 1s where the points need to be pruned.
+          2.b. We then generate a tensor of shape [num_envs, max_points_to_prune] with the ids of the points to prune.
+          2.c. We multiply the two tensors to set the ids of the points to prune to the maximum number of points.
+          2.d. We then convert this tensor to a mask by using one-hot encoding and summing along the points dimension.
+        3. Apply the mask to the ids tensor, the values to be masked are set to the maximum number of points.
+        4. Sort the ids tensor.
+        5. Set the ids above the maximum number of points to 0. This is done so that the extraneous points are set to be
+        the same as the first point in the sequence. This allows for loops to be created.
+
+        Why do we use multinomial sampling over randint? Multinomial sampling allows us to sample without replacement,
+        i.e. we are certain we won't sample the same point twice. This is important because we don't want to prune the
+        same point twice.
+
+        Why do we need step 2.a.? Step 2.a. allows us to prune random number of points without relying on sparse tensors
+        or loops.
+
+        Note:
+        multinomial is faster than argsort(rand) when B > 100. Which is why we use it here.
+
+        Args:
+            points: A 3D tensor of shape [num_envs, num_points, 2].
+
+        Returns:
+            A tuple containing the points and the angles of the tangents.
+            The points are a 3D tensor of shape [num_envs, num_points, 2].
+            The angles are a 2D tensor of shape [num_envs, num_points]."""
+
+        # Sort the points in the trigonometric direction
+        points = self.ccw_sort(points)
+        # For each point sequence, randomly select a number of points to prune
+        # 1. Generate the ids for the points
+        ids = torch.arange(points.shape[1], device=self._device).view(1, -1).expand(points.shape[0], -1)
+        # 2. We want to set some of these ids to the maximum number of points so that they can be pruned
+        max_points_to_prune = self._max_num_points - self._min_num_points
+        # 2.a. First we pick a random number of points to prune, between 0 and max_points_to_prune
+        # It's expressed as a fixed shape tensor to enable batch operations.
+        # Using a multinomial distribution here would result in an uneven distribution of the number of points pruned.
+        num = torch.randint(0, max_points_to_prune, (points.shape[0],), device=self._device)
+        x = torch.arange(max_points_to_prune, device=self._device).unsqueeze(0).expand(points.shape[0], -1)
+        pruning_mask = torch.ones((points.shape[0], max_points_to_prune), device=self._device, dtype=torch.int)
+        pruning_mask[x > num.unsqueeze(1)] = 0
+        # 2.b. We then sample the ids to prune
+        weights = torch.ones((points.shape[0], points.shape[1] - 1), device=self._device)
+        ids_to_prune = torch.multinomial(weights, num_samples=max_points_to_prune, replacement=False)
+        # 2.c. We then multiply the two so that points that don't have to be pruned are set to the maximum number of points + 1
+        # This way we can easily filter them out later
+        final_ids_to_prune = pruning_mask * ids_to_prune + (1 - pruning_mask) * (self._max_num_points - 1)
+        # 2.d. Convert them to a mask using one-hot encoding and summing along the points dimension allows to create a mask
+        ids_mask = torch.sum(torch.nn.functional.one_hot(final_ids_to_prune + 1, self._max_num_points + 1), dim=1)
+        # The values above the maximum number of points are removed
+        ids_mask = ids_mask[:, :-1]
+        # 3. Apply the mask to the ids, the values to be masked are set to the maximum number of points
+        ids = ids * (1 - ids_mask) + self._max_num_points * ids_mask
+        # 4. Sort the ids
+        ids, _ = torch.sort(ids, dim=1)
+        # 5. Set the ids above the maximum number of points to 0
+        ids[ids >= self._max_num_points] = 0
+        # 6. Gather the points
+        points = torch.gather(points, 1, ids.unsqueeze(-1).expand(-1, -1, points.size(2)))
+        # 7. Get the number of points per track
+        num_points_per_track = self._max_num_points - torch.sum(ids_mask == 1, dim=1)
+
+        # Add the first point to the end to close the loop
+        points = torch.cat([points, points[:, 0, :].unsqueeze(1)], dim=1)
+        # Compute the difference between the points
+        dist = torch.diff(points, dim=1)
+        # Compute the angle between the points
+        ang = torch.arctan2(dist[:, :, 1], dist[:, :, 0])
+        # Make sure the angles are in the range [0, 2*pi]
+        ang = self.cast_to_0_2pi(ang)
+        # Compute the angle of the tangents
+        ang1 = ang
+        ang2 = torch.roll(ang, 1, dims=1)
+        # Manually fix the broken roll operation:
+        # adding the first element at the end multiple times screws up the roll
+        ang2[torch.arange(ang2.shape[0]), 0] = ang1[torch.arange(ang1.shape[0]), num_points_per_track - 1]
+        ang = self._p * ang1 + (1 - self._p) * ang2 + (torch.abs(ang2 - ang1) > np.pi) * np.pi
+        # Manually fix the broken roll operation:
+        # adding the first element at the end multiple times screws up the roll
+        ang = torch.cat([ang, ang[:, 0].unsqueeze(1)], dim=1)
+        ang[torch.arange(ang.shape[0]), num_points_per_track] = ang[torch.arange(ang.shape[0]), 0]
+        return points[:, :-1], ang[:, :-1], num_points_per_track
+
     @staticmethod
     def compute_angle(points: torch.Tensor) -> torch.Tensor:
         """
@@ -178,18 +272,8 @@ class TrackGenerator:
 
         # Sort the points in the trigonometric direction
         points = self.ccw_sort(points)
-        # Create 3 points ordered in sequence
-        p0 = torch.roll(points, -1, dims=1)
-        p1 = points
-        p2 = torch.roll(points, 1, dims=1)
-        # Generate 2 vectors from them
-        v1 = p1 - p0
-        v2 = p1 - p2
-        # Compute the angle between them
-        angles = torch.arccos(
-            torch.einsum("ijk,ijk->ij", v1, v2) / (torch.linalg.norm(v1, axis=2) * torch.linalg.norm(v2, axis=2))
-        )
-        return angles
+        # Compute the angle between the points
+        return self.compute_angle(points)
 
     def get_segment(
         self, point_1: torch.Tensor, point_2: torch.Tensor, angle_1: torch.Tensor, angle_2: torch.Tensor
@@ -279,18 +363,73 @@ class TrackGenerator:
         curve = torch.cat(segments, dim=1)
         return curve
 
-    def generate_tracks(self, num_tracks: int) -> dict:
+    def get_bezier_curve_non_fixed_points(
+        self, points: torch.Tensor, angles: torch.Tensor, num_points_per_track: torch.Tensor
+    ) -> torch.Tensor:
+        """Given an array of points, create a curve through those points.
+        Unlike get_bezier_curve, this function can handle tracks with a variable number of points.
+
+        Args:
+            points: A 3D tensor of shape [num_envs, num_points, 2].
+            angles: A 2D tensor of shape [num_envs, num_points].
+            num_points_per_track: A 1D tensor of shape [num_envs] containing the number of points in each track.
+
+        Returns:
+            A tensor of shape [num_envs, num_points*num_points_per_segment, 2]."""
+
+        # For each ordered pair of points, compute the bezier curve that passes through them
+        segments = []
+        for i in range(points.shape[1] - 1):
+            seg = self.get_segment(points[:, i, :2], points[:, i + 1, :2], angles[:, i], angles[:, i + 1])
+            # Perform the check only if the number of points is greater than the minimum number of points
+            if i >= self._min_num_points:
+                overflow = num_points_per_track < i
+                seg[overflow, :, :] = segments[0][overflow, 0].unsqueeze(1)
+            segments.append(seg)
+        seg = self.get_segment(points[:, -1, :2], points[:, 0, :2], angles[:, -1], angles[:, 0])
+        overflow = num_points_per_track < (i + 1)
+        seg[overflow, :, :] = segments[0][overflow, 0].unsqueeze(1)
+        segments.append(seg)
+        curve = torch.cat(segments, dim=1)
+        return curve
+
+    def generate_tracks(self, num_tracks: int) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         """Generate random tracks.
 
         Args:
             num_tracks: The number of tracks to generate.
 
         Returns:
-            A dictionary containing the tracks."""
+            A tuple containing the points, the tangents, and the curve.
+            The points are a 3D tensor of shape [num_tracks, num_points, 2].
+            The tangents are a 2D tensor of shape [num_tracks, num_points].
+            The curve is a 3D tensor of shape [num_tracks, num_points*num_points_per_segment, 2]."""
 
         points, tangents = self.generate_tracks_points(num_tracks)
         curve = self.get_bezier_curve(points, tangents)
         return points, tangents, curve
+
+    def generate_tracks_non_fixed_points(
+        self, num_tracks: int
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+        """Generate random tracks.
+        Unlike generate_tracks, this function can will generate tracks with a variable number of corners.
+        This will result in a larger diversity of tracks generated. Usually, the fewer the number of corners, the
+        simpler the track.
+
+        Args:
+            num_tracks: The number of tracks to generate.
+
+        Returns:
+            A tuple containing the points, the tangents, the number of points per track, and the curve.
+            The points are a 3D tensor of shape [num_tracks, num_points, 2].
+            The tangents are a 2D tensor of shape [num_tracks, num_points].
+            The number of points per track is a 1D tensor of shape [num_tracks].
+            The curve is a 3D tensor of shape [num_tracks, num_points*num_points_per_segment, 2]."""
+
+        points, tangents, num_points_per_track = self.generate_tracks_points_non_fixed_points(num_tracks)
+        curve = self.get_bezier_curve_non_fixed_points(points, tangents, num_points_per_track)
+        return points, tangents, num_points_per_track, curve
 
     def generate_tracks_points(
         self, num_tracks: int, prev_points: torch.Tensor = None
@@ -302,7 +441,9 @@ class TrackGenerator:
             prev_points: The previous points to append the new points to.
 
         Returns:
-            A tuple containing the points and the tangents."""
+            A tuple containing the points and the tangents.
+            The points are a 3D tensor of shape [num_tracks, num_points, 2].
+            The tangents are a 2D tensor of shape [num_tracks, num_points]."""
         # Generate twice as many tracks as needed.
         # This is because some tracks may be discarded if they do not meet the minimum angle requirement.
         num_tracks_tmp = num_tracks * 2
@@ -331,6 +472,52 @@ class TrackGenerator:
             points, tangents = self.get_curve_tangents(points)
         return points, tangents
 
+    def generate_tracks_points_non_fixed_points(
+        self, num_tracks: int, prev_points: torch.Tensor = None
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        """Generate random tracks but only return the points.
+        Unlike generate_tracks_points, this function will prune a random number of points from the input points tensor.
+
+        Args:
+            num_tracks: The number of tracks to generate.
+            prev_points: The previous points to append the new points to.
+
+        Returns:
+            A tuple containing the points, the tangents, and the number of points per track.
+            The points are a 3D tensor of shape [num_tracks, num_points, 2].
+            The tangents are a 2D tensor of shape [num_tracks, num_points].
+            The number of points per track is a 1D tensor of shape [num_tracks]."""
+        # Generate twice as many tracks as needed.
+        # This is because some tracks may be discarded if they do not meet the minimum angle requirement.
+        num_tracks_tmp = num_tracks * 2
+        if num_tracks_tmp < 100:
+            num_tracks_tmp = 100
+        # Generate random points
+        points = self.get_random_points(num_tracks_tmp)
+        # Check if the angles created by the different segments are greater than the minimum angle allowed
+        angles = self.compute_angle_unsorted(points)
+        keep = torch.prod(angles > self._min_angle, dim=1) != 0
+        # Keep only the tracks that meet the minimum angle requirement
+        points = points[keep]
+        # Get the number of points in the previous batch
+        if prev_points is not None:
+            num_prev = prev_points.shape[0]
+            points = torch.cat([prev_points, points], dim=0)
+        else:
+            num_prev = 0
+        # If the number of points is less than the number of tracks, generate more points
+        tangents = None
+        num_points_per_track = None
+        if (keep.sum() + num_prev) < num_tracks:
+            points, tangents, num_points_per_track = self.generate_tracks_points_non_fixed_points(
+                num_tracks, prev_points=points
+            )
+        # If the number of points is greater than the number of tracks, discard the extra points
+        if (keep.sum() + num_prev) >= num_tracks:
+            points = points[:num_tracks]
+            points, tangents, num_points_per_track = self.get_curve_tangents_non_fixed_points(points)
+        return points, tangents, num_points_per_track
+
 
 if __name__ == "__main__":
     rad = 0.2
@@ -342,6 +529,19 @@ if __name__ == "__main__":
     num_loops = 1000
 
     TG = TrackGenerator()
+    print("Generating non-fixed points tracks")
+    s = time.time()
+    for i in range(num_loops):
+        points_nfp, tangents_nfp, num_points_per_track, curve_nfp = TG.generate_tracks_non_fixed_points(num_tracks)
+        # points = TG.get_random_points(1000)
+        # points, tangents, num_points_per_track = TG.get_curve_tangents_non_fixed_points(points)
+        # curve = TG.get_bezier_curve_non_fixed_points(points, tangents, num_points_per_track)
+    e = time.time()
+    print("Time taken:", e - s)
+    print("Time per track:", (e - s) / (1000 * num_loops))
+    print("Tracks per second:", 1000 * num_loops / (e - s))
+
+    print("Generating fixed points tracks")
     s = time.time()
     for i in range(num_loops):
         points, tangents, curve = TG.generate_tracks(num_tracks)
@@ -350,6 +550,14 @@ if __name__ == "__main__":
     print("Time per track:", (e - s) / (num_tracks * num_loops))
     print("Tracks per second:", num_tracks * num_loops / (e - s))
 
+    data_nfp = {}
+    for i in range(1000):
+        data_nfp[i] = {}
+        data_nfp[i]["way_points"] = points_nfp[i].cpu()
+        data_nfp[i]["tangent"] = tangents_nfp[i].cpu()
+        data_nfp[i]["trajectory"] = curve_nfp[i].cpu()
+        data_nfp[i]["complexity"] = np.random.rand(1)[0]
+
     data = {}
     for i in range(1000):
         data[i] = {}
@@ -357,6 +565,24 @@ if __name__ == "__main__":
         data[i]["tangent"] = tangents[i].cpu()
         data[i]["trajectory"] = curve[i].cpu()
         data[i]["complexity"] = np.random.rand(1)[0]
+
+    for k in range(20):
+        fig, ax = plt.subplots(N, N, figsize=(20, 20))
+        for i in range(N):
+            for j in range(N):
+                idx = i * N + j + k * N * N
+                x = data_nfp[idx]["trajectory"][:, 0]
+                y = data_nfp[idx]["trajectory"][:, 1]
+                a = data_nfp[idx]["way_points"]
+                ax[i, j].set_aspect("equal")
+                ax[i, j].plot(x, y, lw=2, c="r")
+                ax[i, j].scatter(a[:, 0], a[:, 1], c="k")
+                ax[i, j].scatter(a[1, 0], a[1, 1], c="r")
+                ax[i, j].scatter(a[2, 0], a[2, 1], c="b")
+                ax[i, j].scatter(a[3, 0], a[3, 1], c="g")
+                ax[i, j].set_title("Track #" + str(idx) + ", # corners: " + str(num_points_per_track[idx].item()))
+        plt.savefig("bezier_nfp_" + str(k) + ".png")
+        plt.close()
 
     for k in range(20):
         fig, ax = plt.subplots(N, N, figsize=(20, 20))
